d61c09126cf7e75c3c4ef1c35c37f996ddb037fb
Rohitsoni86 <dynamicsoni.rohit@gmail.com>
2023-09-14 23:42:50 +0530

    DataTypes Mod Added

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,12 @@
+let myNum = 777; // Number
+let myName = "Rohit"; //String
+let todayIsSunday = true; //Boolean
+let currentTemp = undefined; // undefined
+let riyaResponse = null; //null value
+let Student = {
+  Name: "Tarun",
+  Phn: 77777777,
+};
+
+console.log(Student);
+console.log(typeof Student); //OBj
